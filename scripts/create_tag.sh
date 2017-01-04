31aaf8af90e1f4188c10a9045d101d41d0cece2c
Matt Bentley <mbentley@mbentley.net>
2017-01-04 15:06:04 -0500

    Fixed tag creation
    Signed-off-by: Matt Bentley <mbentley@mbentley.net>

--- scripts/create_tag.sh
+++ scripts/create_tag.sh
@@ -3,7 +3,6 @@
 set -e
 
 VER="${1:-}"
-GIT_SHA="$(git rev-parse --short=6 HEAD)"
 export GPG_TTY="$(tty)"
 
 # check to see if a version was provided
@@ -13,12 +12,12 @@ then
   echo "Last 3 tags:"
   git tag | tail -n 3
   # prompt user for version
-  echo -ne "\nProvide a version number (e.g. - 1.2.3-0): "
+  echo -ne "\nProvide a version number (e.g. - v1.2.3): "
   read VER
 fi
 
 # create a new tag
-git tag -s -a v${VER}-${GIT_SHA} -m "v${VER}-${GIT_SHA}"
+git tag -s -a ${VER} -m "${VER}"
 
 # notify user of new tag
-echo "Created tag '$(git tag | grep v${VER}-${GIT_SHA})'"
+echo "Created tag '$(git tag | grep ${VER})'"
