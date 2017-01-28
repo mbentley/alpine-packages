81608ac5c71e27ed5f0ea1cafe9c2057d590a877
Matt Bentley <mbentley@mbentley.net>
2017-01-28 18:03:12 -0500

    Updated tag script
    Signed-off-by: Matt Bentley <mbentley@mbentley.net>

--- scripts/create_tag.sh
+++ scripts/create_tag.sh
@@ -5,6 +5,10 @@ set -e
 VER="${1:-}"
 export GPG_TTY="$(tty)"
 
+# fetch all tagsto make sure we have a lit of the latest
+echo "Fetching all tags..."
+git fetch --tags
+
 # check to see if a version was provided
 if [ -z "${VER}" ]
 then
@@ -21,3 +25,7 @@ git tag -s -a ${VER} -m "${VER}"
 
 # notify user of new tag
 echo "Created tag '$(git tag | grep ${VER})'"
+
+# push tags
+echo "Pushing tags..."
+git push origin --tags
